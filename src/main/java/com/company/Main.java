8abe4909982796eebe4b93f57c635493830e0e07
ubeyid <50629713+ubeyid@users.noreply.github.com>
2022-05-02 23:14:16 +0300

    Update Main.java

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -4,8 +4,6 @@ public class Main{
 
     public static void main(String[] args) {
 
-     System.setProperty("javax.net.ssl.keyStore", "za.store");
-     System.setProperty("javax.net.ssl.keyStorePassword", "ubeyid");
       new Thread(new ProxyServer(Utils.SERVER_PORT_TO_LISTEN)).start();
 
     }
